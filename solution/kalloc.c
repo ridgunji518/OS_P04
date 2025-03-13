6b0f718918e6d1d8745bfd52dd37139c257bc08d
Samarth Boranna <boranna@royal-14.cs.wisc.edu>
2025-03-13 13:48:11 -0500

    added function definitions to defs.h and in kalloc.c

--- solution/kalloc.c
+++ solution/kalloc.c
@@ -10,6 +10,7 @@
 #include "spinlock.h"
 
 void freerange(void *vstart, void *vend);
+void freerange_huge(void *vstart, void *vend);
 extern char end[]; // first address after kernel loaded from ELF file
                    // defined by the kernel linker script in kernel.ld
 
