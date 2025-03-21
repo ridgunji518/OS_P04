7970028fad575fb8d21266f7b84a88a075947d31
Rahul Idgunji <idgunji@royal-14.cs.wisc.edu>
2025-03-20 22:49:46 -0500

    idgunji/ Added growproc

--- solution/proc.c
+++ solution/proc.c
@@ -161,15 +161,29 @@ growproc(int n, int flag)
   uint sz;
   struct proc *curproc = myproc();
 
-  sz = curproc->sz;
-  if(n > 0){
-    if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
-      return -1;
-  } else if(n < 0){
-    if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
-      return -1;
+  if(flag == 0){
+    sz = curproc->sz;
+    if(n > 0){
+      if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
+        return -1;
+    } else if(n < 0){
+      if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
+        return -1;
+    }
+    curproc->sz = sz; 
   }
-  curproc->sz = sz;
+  else{
+    sz = curproc->hugesz;
+    if(n > 0){
+      if((sz = allocuvm(curproc->pgdir, HUGE_VA_OFFSET + sz, HUGE_VA_OFFSET + sz + n)) == 0)
+        return -1;
+    }
+    else if(n < 0){
+      if((sz = deallocuvm(curproc->pgdir, HUGE_VA_OFFSET + sz, HUGE_VA_OFFSET + sz + n)) == 0)
+        return -1;
+    }
+    curproc->hugesz = sz;
+  }
   switchuvm(curproc);
   return 0;
 }
